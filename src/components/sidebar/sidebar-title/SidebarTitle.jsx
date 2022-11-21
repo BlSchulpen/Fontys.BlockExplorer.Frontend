9ab1899eb5131798dfb2adf459670a3d5747f1b7
Bram Schulpen <bram.schulpen@live.nl>
2022-11-21 15:48:01 +0100

    created logo

--- src/components/sidebar/sidebar-title/SidebarTitle.jsx
+++ src/components/sidebar/sidebar-title/SidebarTitle.jsx
@@ -1,8 +1,18 @@
-import { Link } from "react-router-dom";
 import "./sidebar-title.scss";
+import { Link } from "react-router-dom";
 
 function SidebarTitle() {
     return (
+//     <Toolbar>
+//     <Box
+//         component="img"
+//         sx={{
+//         height: 0,
+//         width:0
+//         }}
+//         alt="Your logo."
+//         src= {"../../../../resources/blockexplorer.jpg"}/>
+// </Toolbar>
         <div className="sidebar-title">
             <Link to="/" style={{ textDecoration: "none" }}>
             <span className="logo">Block explorer</span>
